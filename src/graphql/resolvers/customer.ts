81100cbb036740b6e8782af6abd4df03053cc1ae
Michael Huff <huff600@hotmail.com>
2025-06-16 12:29:44 -0600

    Forgot to run pnpm dev

--- src/graphql/resolvers/customer.ts
+++ src/graphql/resolvers/customer.ts
@@ -1,34 +1,37 @@
 // src/graphql/resolvers/customer.ts
 import {
-    validateCustomer,
-    validateCustomerUpdate,
-    CustomerInput,
-    CustomerUpdateInput
-} from '../../schemas/customer.zod.js';
-import { Customer } from '../../models/customer.js';
+  validateCustomer,
+  validateCustomerUpdate,
+  CustomerInput,
+  CustomerUpdateInput,
+} from "../../schemas/customer.zod.js";
+import { Customer } from "../../models/customer.js";
 
 export const customerResolvers = {
-    Query: {
-        customers: async () => {
-            return await Customer.find();
-        },
-        customer: async (_: unknown, { id }: { id: string }) => {
-            return await Customer.findById(id);
-        },
+  Query: {
+    customers: async () => {
+      return await Customer.find();
     },
-    Mutation: {
-        createCustomer: async (_: unknown, { input }: { input: CustomerInput }) => {
-            const validatedData = validateCustomer(input);
-            const customer = new Customer(validatedData);
-            return await customer.save();
-        },
-        updateCustomer: async (_: unknown, { id, input }: { id: string; input: CustomerUpdateInput }) => {
-            const validatedData = validateCustomerUpdate(input);
-            return await Customer.findByIdAndUpdate(id, validatedData, { new: true });
-        },
-        deleteCustomer: async (_: unknown, { id }: { id: string }) => {
-            await Customer.findByIdAndDelete(id);
-            return true;
-        },
+    customer: async (_: unknown, { id }: { id: string }) => {
+      return await Customer.findById(id);
     },
+  },
+  Mutation: {
+    createCustomer: async (_: unknown, { input }: { input: CustomerInput }) => {
+      const validatedData = validateCustomer(input);
+      const customer = new Customer(validatedData);
+      return await customer.save();
+    },
+    updateCustomer: async (
+      _: unknown,
+      { id, input }: { id: string; input: CustomerUpdateInput }
+    ) => {
+      const validatedData = validateCustomerUpdate(input);
+      return await Customer.findByIdAndUpdate(id, validatedData, { new: true });
+    },
+    deleteCustomer: async (_: unknown, { id }: { id: string }) => {
+      await Customer.findByIdAndDelete(id);
+      return true;
+    },
+  },
 };
--- src/models/customer.ts
+++ src/models/customer.ts
@@ -2,33 +2,37 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IOauthProvider {
-    _id: mongoose.Types.ObjectId;
-    provider_id: string;
-    email: string;
-    created_at: Date;
-    updated_at: Date;
+  _id: mongoose.Types.ObjectId;
+  provider_id: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export interface ICustomer extends Document {
-    _id: mongoose.Types.ObjectId;
-    org_name: string;
-    org_handle: string;
-    timezone: string;
-    email: string;
-    phone: string;
-    oauth_providers: IOauthProvider[];
-    account_setup_complete: boolean;
-    marketing_consent: boolean;
-    created_at: Date;
-    updated_at: Date;
+  _id: mongoose.Types.ObjectId;
+  org_name: string;
+  org_handle: string;
+  timezone: string;
+  email: string;
+  phone: string;
+  oauth_providers: IOauthProvider[];
+  account_setup_complete: boolean;
+  marketing_consent: boolean;
+  created_at: Date;
+  updated_at: Date;
 }
 
-const OauthProviderSchema = new Schema<IOauthProvider>({
+const OauthProviderSchema = new Schema<IOauthProvider>(
+  {
     provider_id: { type: String, required: true },
     email: { type: String, required: false },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-const CustomerSchema = new Schema<ICustomer>({
+const CustomerSchema = new Schema<ICustomer>(
+  {
     org_name: { type: String, required: true },
     org_handle: { type: String, required: true },
     timezone: { type: String, required: true },
@@ -37,6 +41,8 @@ const CustomerSchema = new Schema<ICustomer>({
     oauth_providers: { type: [OauthProviderSchema], required: true },
     account_setup_complete: { type: Boolean, default: false },
     marketing_consent: { type: Boolean, default: false },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 export const Customer = mongoose.model<ICustomer>("Customer", CustomerSchema);
